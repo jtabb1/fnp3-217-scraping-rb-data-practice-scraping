7e628de525672f47e632fcba0eeacf17fe88b2ff
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-07-30 10:40:38 -0400

    Complete code along

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,27 @@
+# Code along with the code mostly given in the problem statement
+
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = URI.open("https://flatironschool.com/")
+
+
+doc = Nokogiri::HTML(html)
+str = doc.css(".headline-26OIBN").text
+puts str
+puts str[str.size-1]
+
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+courses.each{|et| puts et.text.strip}
+
+puts
+puts
+p doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0]
+
+puts
+puts
+p doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0].name
+
+puts
+puts
+p doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0].attributes
